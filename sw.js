1008b575cdd7f56bedecacdb0409c7f9992f66c5
dbrinda <dbrinda@gmail.com>
2026-05-13 19:44:48 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
-const V = 'mf-v1';
-const FILES = ['./', './index.html'];
+const V = ‘mf-v1’;
+const FILES = [’./’, ‘./index.html’];
 
-self.addEventListener('install', e =>
-  e.waitUntil(
-    caches.open(V).then(c => c.addAll(FILES))
-  )
+self.addEventListener(‘install’, e =>
+e.waitUntil(
+caches.open(V).then(c => c.addAll(FILES))
+)
 );
 
-self.addEventListener('activate', e =>
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== V).map(k => caches.delete(k)))
-    )
-  )
+self.addEventListener(‘activate’, e =>
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== V).map(k => caches.delete(k)))
+)
+)
 );
 
-self.addEventListener('fetch', e =>
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  )
+self.addEventListener(‘fetch’, e =>
+e.respondWith(
+caches.match(e.request).then(r => r || fetch(e.request))
+)
 );
